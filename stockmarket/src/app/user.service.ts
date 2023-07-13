79687adadb517393c7b702a520b611b64e1c3c23
Abdelbaset65 <omarabdelbaset99@outlook.com>
2023-07-13 14:55:40 +0300

    akvbnlkmv

--- stockmarket/src/app/user.service.ts
+++ stockmarket/src/app/user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  private url = 'http://localhost:5089/api/User';
+
+  signIn(user: User): Observable<any> {
+    const url = `${this.url}/signin`;
+    return this.http.post(url, user);
+  }
+
+  signUp(user: User): Observable<any> {
+    const url = `${this.url}/signup`;
+    return this.http.post(url, user);
+  }
 }
